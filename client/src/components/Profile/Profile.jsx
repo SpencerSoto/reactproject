27a29a0f83ed5567bbc34d29445bd2f51063536f
Spencer Soto <spencer.soto@gmail.com>
2021-06-29 21:58:54 -0400

    user component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -20,43 +20,64 @@ import "./Profile.css";
  
  
 class Profile extends Component {
-   render() {
-       console.log(this.props.pins)
-       const pins = this.props.pins?.map(pin => <h5 key={pin._id}>{pin.description}</h5>)
-       console.log(pins)
-       return (
-                     
-           <div class=" profile">
-               <div>
-                   <img style={{width:"160px", height:"160px",borderRadius:"80px"}}
-                   src="https://play-lh.googleusercontent.com/TECjsAu9DWLbKBuDl2sDHEUhlTh-n0eTgrHbt-7-a33Fy2p-slkHg8FN6r_dCbKBdA"
-                   />
-                   <h2>Profile for Wingz4Lyfe</h2>
-               </div>
-               <h1 id='Profile'>Profile</h1>
-               <h3>User Info: Spencer Soto</h3>
-               <h4>Location: Miami</h4> <br></br>
-              <p> Our community is dedicated to the Adventures of your Life!</p>
- 
-               <div class="profile-work">
-                  
-                   <h2>PINS</h2>
-                   {/* {PINS = this.state.user}ß */}
-                   {pins}
-                   </div>
-               <div className="profile-work">
-                   <h2>Blogs</h2>
-                   {/* {blogs} */}
-               {/* user={this.state.user} */}
-               </div>
-           </div>
- 
-           // {/* <EditProfile/> */}
-         
- 
-       )
-   }
-}
- 
- 
-export default Profile;
+    // const ProfilePage = () => (
+    //     <User
+    //       render={({ user, error, loading }) => {
+    //         if (error) return <p>{error}</p>;
+    //         if (loading) return <p>Loading...</p>;
+    //         return (
+    //           <div className="userProfile">
+    //             <img src={user.picture.large} alt="Profile Picture" />
+    //             <h1>{user.name}</h1>
+    //             <div className="userInfo">
+    //               <ul>
+    //                 <li>Email: {user.email}</li>
+    //                 <li>Username: {user.login.username}</li>
+    //                 <li>Phone: {user.phone}</li>
+    //                 <li>Birthdate: {user.dob.date}</li>
+    //               </ul>
+    //             </div>
+    //           </div>
+    //         );
+    //       }}
+    //     />
+    //   );
+      render() {
+        console.log(this.props.pins)
+        const pins = this.props.pins?.map(pin => <h5 key={pin._id}>{pin.description}</h5>)
+        console.log(pins)
+        return (
+                      
+            <div class=" profile">
+                <div>
+                    <img style={{width:"160px", height:"160px",borderRadius:"80px"}}
+                    src="https://play-lh.googleusercontent.com/TECjsAu9DWLbKBuDl2sDHEUhlTh-n0eTgrHbt-7-a33Fy2p-slkHg8FN6r_dCbKBdA"
+                    />
+                    <h2>Profile for Wingz4Lyfe</h2>
+                </div>
+                <h1 id='Profile'>Profile</h1>
+                <h3>User Info: Spencer Soto</h3>
+                <h4>Location: Miami</h4> <br></br>
+               <p> Our community is dedicated to the Adventures of your Life!</p>
+  
+                <div class="profile-work">
+                   
+                    <h2>PINS</h2>
+                    {/* {PINS = this.state.user}ß */}
+                    {pins}
+                    </div>
+                <div className="profile-work">
+                    <h2>Blogs</h2>
+                {/* user={this.state.user} */}
+                </div>
+            </div>
+  
+            // {/* <EditProfile/> */}
+          
+  
+        )
+    }
+ }
+  
+  
+ export default Profile;
