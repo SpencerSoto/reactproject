33314852f81bd08c5d26acad3f9d88df90e36c65
Matthew Solomon <matthewsolomon@Matthews-MBP.fios-router.home>
2021-07-03 09:24:01 -0400

    updating home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,9 +3,13 @@ import "../App.css";
 function HomePage() {
   return (
     <div className="App">
-   <div className="homepage-container">
-     <h1>Hello</h1>
-   </div>
+      <div className="homepage-container">
+        <br></br>
+        <br></br>
+        <h1>Welcome to TravelBuds!</h1>
+        <h2>Where will you explore on your next journey?</h2>
+        <h2>Let's get started!</h2>
+      </div>
     </div>
   );
 }
